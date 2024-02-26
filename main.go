471b532cc3426341fa275cb79e6231f6c34e5c1b
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-02-26 17:03:54 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	fmt.Println("Hej hej kolla vad coolt")
 	t := tabby.New()
-	t.AddHeader("NAME", "TITLE", "DEPARTMENT")
+	t.AddHeader("NAME", "TITLE", "DEPARTMENT2")
 	t.AddLine("John Smith", "Developer", "Engineering")
 	t.AddLine("Stefan Holmberg", "Developer", "Whatever")
 	t.Print()
